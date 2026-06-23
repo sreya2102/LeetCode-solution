8800d7642541e8e99d9de8694bce5d3cd40538b2
sreya2102 <sreyaprathap0306@gmail.com>
2026-06-23 21:39:38 +0530

    Remove Hashing folder

--- Hashing/Contains duplicate.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        n=len(nums)
-        m=set(nums)
-        a=len(m)
-        if n!=a:
-            return True
-        return False
