f83e6cbce5c02c26f5107b6069ff346033829964
sreya2102 <sreyaprathap0306@gmail.com>
2026-09-10 15:19:48 +0530

    Add dynamic programming solution

---
+++ Dynamic programming/Coin Change II.py
@@ -0,0 +1,16 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        n=len(coins)
+        dp=[[0]*(amount+1) for _ in range(len(coins))]
+        for i in range(amount+1):
+            if i%coins[0]==0:
+                dp[0][i]=1
+        for i in range(len(coins)):
+            dp[i][0]=1
+        for i in range(1,len(coins)):
+            for j in range(1,amount+1):
+                if j>=coins[i]:
+                    dp[i][j]=dp[i-1][j]+dp[i][j-coins[i]]
+                else:
+                    dp[i][j]=dp[i-1][j]
+        return dp[len(dp)-1][len(dp[0])-1]
