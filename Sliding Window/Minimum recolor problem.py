f353daa02f13af1a3c32d57409e970f5c25189d5
sreya2102 <sreyaprathap0306@gmail.com>
2026-09-10 15:21:07 +0530

    Update solution for Minimum recolor problem

--- Sliding Window/Minimum recolor problem.py
+++ Sliding Window/Minimum recolor problem.py
@@ -3,15 +3,4 @@ class Solution:
         s=blocks
         n=len(s)
         c=0
-        for i in range(k):
-            if s[i]=='W':
-                c=c+1
-        mi=c
-        for i in range(1,n-k+1):
-            if s[i-1]=='W':
-                c=c-1
-            if s[i+k-1]=='W':
-                c=c+1
-            if c<mi:
-                mi=c
-        return mi
+        f
