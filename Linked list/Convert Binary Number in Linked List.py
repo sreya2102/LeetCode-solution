7dd01e0964d08775703c4d67393f4584e4500a34
sreya2102 <sreyaprathap0306@gmail.com>
2026-09-11 09:26:27 +0530

    Add Linked List Solution

---
+++ Linked list/Convert Binary Number in Linked List.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: Optional[ListNode]) -> int:
+        res=""
+        temp=head
+        while temp:
+            res=res+str(temp.val)
+            temp=temp.next
+        return int(res,2)
+        
