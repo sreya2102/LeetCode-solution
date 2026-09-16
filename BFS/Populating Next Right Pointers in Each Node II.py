68c8be30662cf525f19aa9b7a28f457af28d3d69
sreya2102 <sreyaprathap0306@gmail.com>
2026-09-16 16:24:47 +0530

    Add BFS solution

---
+++ BFS/Populating Next Right Pointers in Each Node II.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if root is None:
+            return None
+        queue=deque()
+        queue.append(root)
+        while queue:
+            prev=None
+            for i in range(len(queue)):
+                node=queue.popleft()
+                if prev:
+                    prev.next=node
+                prev=node
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            prev.next=None
+        return root
